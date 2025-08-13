86d42f6e25f8dbbe5f95f5a263f302cfdf33ff8d
hudazain04 <huda1812zain@gmail.com>
2025-08-13 23:18:13 +0300

    role middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -25,7 +25,7 @@ class RoleMiddleware
 
             $userRoles = $user->getRoleNames()->toArray();
             $hasRole = false;
-            dd($roles);
+//            dd($roles);
             foreach ($roles as $role) {
                 if (in_array($role, $userRoles)) {
                     $hasRole = true;
