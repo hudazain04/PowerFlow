8c5420b3ff549e508758827af5e2cf767aa0d02e
mohamd-wanli <wlmohamd1234@gmail.com>
2025-09-10 19:38:39 +0300

    types of counter

--- app/Types/CounterStatus.php
+++ app/Types/CounterStatus.php
@@ -7,10 +7,15 @@ class CounterStatus
     public  const InCheck= 'InCheck';
     public  const Ready = "Ready";
     public const Failed = "Failed";
+    public const Connect = "Connected";
+        public const DisConnected="DisConnected";
     public static array $CounterTypes= [
        self::InCheck,
         self::Ready,
-        self::Failed
+        self::Failed,
+        self::Connect,
+        self::DisConnected
+
     ];
 
 }
