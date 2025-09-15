c35f7319debf8d402b6f13ad1f906917089a8f95
mohamd-wanli <wlmohamd1234@gmail.com>
2025-09-15 14:06:01 +0300

    device_id

--- database/migrations/2025_03_23_080627_create_counters_table.php
+++ database/migrations/2025_03_23_080627_create_counters_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('number');
             $table->string('QRCode');
             $table->integer('current_spending');
+            $table->string('physical_device_id')->nullable();
             $table->enum('spendingType',array_values((new ReflectionClass(\App\Types\SpendingTypes::class))->getConstants()));
             $table->foreignId('generator_id')->nullable()->constrained('power_generators')->nullOnDelete();
             $table->string('status')->default(\App\Types\CounterStatus::InCheck);
