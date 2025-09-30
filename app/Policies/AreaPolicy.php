1549bd2c522544810d6d337a0b58b97587498469
mohamd-wanli <wlmohamd1234@gmail.com>
2025-09-30 17:08:21 +0300

    edit consumption

---
+++ app/Policies/AreaPolicy.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Area;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class AreaPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasPermission('VIEW_NEIGHBORHOODS');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Area $area): bool
+    {
+        return $user->hasPermission('VIEW_NEIGHBORHOOD') &&
+            $user->powerGenerator->id === $area->generator_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Area $area): bool
+    {
+        return $user->hasPermission('UPDATE_NEIGHBORHOODS') &&
+            $user->powerGenerator->id === $area->generator_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Area $area): bool
+    {
+        return $user->hasPermission('DELETE_NEIGHBORHOODS') &&
+            $user->powerGenerator->id === $area->generator_id;
+    }
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Area $area): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Area $area): bool
+    {
+        return false;
+    }
+}
