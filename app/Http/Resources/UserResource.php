210ab4ffc08186243f086059879731f9830db45e
hudazain04 <huda1812zain@gmail.com>
2025-10-03 15:59:49 +0300

    first , last name

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,8 +17,8 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-//            'first_name' => $this->first_name,
-//            'last_name' => $this->last_name,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
             'full_name'=>$this->fullName(),
             'email' => $this->email,
 //            'password'=>$this->password,
