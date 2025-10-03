8fb5980b198b6d1f73efbce3cbe6aade6056ff85
Jawad2003Taki <jawad2002taki@gmail.com>
2025-10-03 11:18:44 +0300

    add server seeder

---
+++ database/seeders/ServerSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ServerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            SuperAdminAccountSeeder::class
+        ]);
+    }
+}
