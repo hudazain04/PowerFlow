e8812b0896ce263f36f21b4a4af1c2bd4aa1e3e6
hudazain04 <huda1812zain@gmail.com>
2025-10-26 03:12:56 +0300

    k:

--- app/Services/Admin/ActionService.php
+++ app/Services/Admin/ActionService.php
@@ -110,7 +110,7 @@ class ActionService
                 'generator_id'=>$action->generator_id,
             ]);
             $counter=$action->counter;
-            $response = Http::post(env('ESP_URL') . '/relay/rabbitmq/disconnect/'.$counter->physical_device_id);
+            $response = Http::post(env('ESP_URL') . '/relay/rabbitmq/connect/'.$counter->physical_device_id);
 //            if ($response->successful()) {
 //               $action=$this->actionRepository->update($newAction,[
 //                  'status' =>ComplaintStatusTypes::Resolved,
@@ -127,7 +127,7 @@ class ActionService
 //               'status'=>ComplaintStatusTypes::Accepted,
 //           ]);
            $counter=$action->counter;
-           $response = Http::post(env('ESP_URL') . '/relay/rabbitmq/connect/'.$counter->physical_device_id);
+           $response = Http::post(env('ESP_URL') . '/relay/rabbitmq/disconnect/'.$counter->physical_device_id);
 //           if ($response->successful()) {
 //               $action=$this->actionRepository->update($action,[
 //                   'status' =>ComplaintStatusTypes::Resolved,
